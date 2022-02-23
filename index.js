3cda7e9df07c3587bfcb315eb402bba9e5aeb593
Caio Santos <caio.santos@msn.com>
2022-02-23 21:17:50 +0900

    add app get

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ const baseUrl = `http://api.scraperapi.com?api_key=${process.env.API_KEY_SCRAPER
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello From Express.');
+  res.send('Welcome to Amazon Scraper API!');
+});
+
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(`${baseUrl}&url=${process.env.URL_PRODUCT}/${productId}`);
+    res.send(response);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
