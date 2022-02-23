1a49c15c3b97ab20bc38eaae7a47ab2160446778
Caio Santos <caio.santos@msn.com>
2022-02-23 21:13:18 +0900

    add baseUrl

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const baseUrl = `http://api.scraperapi.com?api_key=${process.env.API_KEY_SCRAPER}&autoparse=true`;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
